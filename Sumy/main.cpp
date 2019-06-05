0affe0f747f3ec71f16ee314e57cff6385bb0af0
Jakub <kontakt@jakub0301.net>
2019-06-05 20:27:32 +0200

    Finished task Sumy

--- Sumy/main.cpp
+++ Sumy/main.cpp
@@ -16,17 +16,31 @@ int main(){
 	}
 	for(int i = 1; i <= n; i++){
 	   for(int j = 1; j <= m; j++){
-		tab[i][j] += tab[i - 1][j] + tab[i][j - 1];   
+		tab[i][j] += tab[i - 1][j] + tab[i][j - 1] - tab[i - 1][j - 1];   
 	   }
 	}
-	
+	/*
+	cout << endl << endl;
 	for(int i = 0; i <= n; i++){
 		for(int j = 0; j <= m; j++){
 			cout << tab[i][j] << " ";
 		}
 		cout << endl;
-	}	
+	}
+	cout << endl << endl << endl;
+	*/
 
+
+	int testow;
+	int a,b,c,d;
+	cin >> testow;
+    //cout<<tab[2][3];
+    for(int i = 0; i < testow; i++){
+	cin >> a >> b >> c >> d;
+	cout << tab[c][d] - tab[a - 1][d] - tab[c][b - 1] + tab[a - 1][b - 1] << endl;
+	//deb
+	//cout << tab[c][d] << " " << tab[a - 1][d] << " " << tab[c - 1][b] << " " << tab[a - 1][b - 1] << endl;
+	}
 
 
 
